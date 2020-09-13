1eb6fc888d13657bdc227075bf745804234e33f5
djeloul abderrezague <mohamed.abderrezague@gmail.com>
2020-09-13 18:34:36 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import myImage from "./ImageInSrc.png";
+import ReactPlayer from "react-player";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{ border: "solid 1px black", maxWidth: "100vw", display:"flex", alignItems:"center", justifyContent:"center", flexDirection:"column" }}>
+        <div className="title red">abderrezague mohamed</div>
+
+        <img src={myImage} alt="srbc" />
+
+        <img src={"/imageInPublic.png"} alt="public" />
+        <ReactPlayer url="https://www.youtube.com/watch?v=ug50zmP9I7s" />
+      </div>
     </div>
   );
 }
